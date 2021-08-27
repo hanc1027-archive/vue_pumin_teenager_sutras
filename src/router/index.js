f52153f6ab8d54c98016acb6c70eba988609b5e7
Nelson Hsu <nelsonhsu1027@gmail.com>
2021-08-28 01:54:00 +0800

    加上普門品經文

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Pu from '../views/Pu.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/pu',
+    name: 'Pu',
+    component: Pu
   },
 ]
 
